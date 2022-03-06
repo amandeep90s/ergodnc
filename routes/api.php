fcbfd6d15ee29132e40e3e8900a83b0d7dc5ee0d
Amandeep Singh <programmeramandeep@gmail.com>
2022-03-06 13:43:34 +0530

    Offices - lists and test

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\OfficeController;
+use App\Http\Controllers\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/tags', \App\Http\Controllers\TagController::class);
+// Tags...
+Route::get('/tags', TagController::class);
+
+// Offices...
+Route::get('/offices', [OfficeController::class, 'index']);
